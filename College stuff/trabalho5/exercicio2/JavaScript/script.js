272cce5d501e6ac017314e757752bda2369912d2
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-03-29 13:21:28 -0300

    Delete College stuff/trabalho5 directory

--- College stuff/trabalho5/exercicio2/JavaScript/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function () {
-    let nome = document.querySelector("h1");
-    nome.addEventListener("click", () => nome.textContent = "Ciclano de Tal")
-    let subtitulos = document.querySelectorAll("h2");
-    for(let node of subtitulos)
-    node.addEventListener("click", () => node.textContent = "Obrigado")
-
-    
-})
