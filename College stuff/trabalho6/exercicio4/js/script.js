d6c3a08c4c7300f18f0feeeef91fa53e76a3dbbe
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-04-06 21:56:44 -0300

    Add files via upload

--- College stuff/trabalho6/exercicio4/js/script.js
+++ College stuff/trabalho6/exercicio4/js/script.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    nav = document.querySelectorAll("nav button");
-    for(let button of buttons){
-        button.addEventListener("click", mudaTab);
+window.onload = ()=>{ //executa a funcao assim que a arvore DOM for carregada
+    botoes = document.querySelectorAll("nav button"); //seleciona os botoes dentro de nav
+    for(let botao of botoes){ //vai de item a item da lista de botoes
+        botao.addEventListener("click", mudaTab); //quando clicar em algum botao valido, sera usada a funcao muda tab passando parametro de listener e
     }
-    abreTab(0);
-})
-function mudaTab(e){
-    const botao = e.target;
-    const lista = botao.parentNode;
-    const nodes = Array.from(lista.parentNode.children);
-    const index = nodes.indexOf(lista);
-    abreTab(index);
+    abreTab(3); //abre-se a tab correspondente
+}
+function mudaTab(e){ //funcao recebe elemento que acordou a funcao
+    const botao = e.target; //botao atribui ao botao que iniciou a funcao
+    const lista = botao.parentNode; //acha a lista por meio do pai do botao, neste caso o div
+    const nodes = Array.from(lista.parentNode.children); //faz isso para ver quantos elementos a lista contem
+    const index = nodes.indexOf(lista); //acha o numero correspondente
+    abreTab(index); //abre a tab com o numero desejado relacionado ao numero de elementos do nav
 }
 
-function abreTab(i){
-    const tabOn = docume.querySelector(".tabOn");
-    if(tabOn !== null)tabOn.className = "";
+function abreTab(i){ //funcao que muda a pagina para o elemento desejado
+    const tabOn = document.querySelector(".tabOn"); //atribui ao elemento cuja classe tem este nome
+    if(tabOn !== null)tabOn.className = ""; //se nao for nulo, seu nome de classe desaparecera
 
-    const buttonOn = document.querySelector(".buttonOn");
-    if(buttonOn !== null) buttonActive.className = "";
+    const buttonOn = document.querySelector(".buttonOn"); //atribui ao elemento cuja classe tem este nome
+    if(buttonOn !== null) buttonActive.className = ""; //se nao for nulo, seu nome de classe desaparecera
 
-    document.querySelectorAll(".tabs section")[i].className = "tabOn";
+    document.querySelectorAll(".tabs section")[i].className = "tabOn"; //se nao existir, vai comecar a existir e ter o nome do valor atribuido
     document.querySelectorAll("nav button")[i].className = "buttonOn";
 
 }
