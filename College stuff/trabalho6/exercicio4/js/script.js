24e97268e9fc0e26c691f4a5dce524a34ad020b1
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-04-07 16:37:02 -0300

    Add files via upload

--- College stuff/trabalho6/exercicio4/js/script.js
+++ College stuff/trabalho6/exercicio4/js/script.js
@@ -1,9 +1,10 @@
 window.onload = ()=>{ //executa a funcao assim que a arvore DOM for carregada
+    document.forms.formContato.onsubmit = validaForm;
     botoes = document.querySelectorAll("nav button"); //seleciona os botoes dentro de nav
     for(let botao of botoes){ //vai de item a item da lista de botoes
         botao.addEventListener("click", mudaTab); //quando clicar em algum botao valido, sera usada a funcao muda tab passando parametro de listener e
     }
-    abreTab(3); //abre-se a tab correspondente
+    abreTab(0); //abre-se a tab correspondente
 }
 function mudaTab(e){ //funcao recebe elemento que acordou a funcao
     const botao = e.target; //botao atribui ao botao que iniciou a funcao
@@ -14,13 +15,38 @@ function mudaTab(e){ //funcao recebe elemento que acordou a funcao
 }
 
 function abreTab(i){ //funcao que muda a pagina para o elemento desejado
-    const tabOn = document.querySelector(".tabOn"); //atribui ao elemento cuja classe tem este nome
-    if(tabOn !== null)tabOn.className = ""; //se nao for nulo, seu nome de classe desaparecera
+    const tabActive = document.querySelector(".tabActive"); //atribui ao elemento cuja classe tem este nome
+    if(tabActive !== null)tabActive.className = ""; //se nao for nulo, seu nome de classe desaparecera
 
-    const buttonOn = document.querySelector(".buttonOn"); //atribui ao elemento cuja classe tem este nome
-    if(buttonOn !== null) buttonActive.className = ""; //se nao for nulo, seu nome de classe desaparecera
+    const buttonActive = document.querySelector(".buttonActive"); //atribui ao elemento cuja classe tem este nome
+    if(buttonActive !== null) buttonActive.className = ""; //se nao for nulo, seu nome de classe desaparecera
 
-    document.querySelectorAll(".tabs section")[i].className = "tabOn"; //se nao existir, vai comecar a existir e ter o nome do valor atribuido
-    document.querySelectorAll("nav button")[i].className = "buttonOn";
+    document.querySelectorAll(".tabs section")[i].className = "tabActive"; //se nao existir, vai comecar a existir e ter o nome do valor atribuido
+    document.querySelectorAll("nav button")[i].className = "buttonActive";
 
+}
+
+function validaForm(e){
+    let form = e.target; //atribui o form para a variavel
+    let formValido = true; //variavel de validacao verdadeira
+    const spanUser = form.usuario.nextElementSiblin; //acha o span do usuario
+    const spanSenha = form.senha.nextElementSiblin; //acha o span da senha
+    const spanEmail = form.email.nextElementSiblin; //acha o span do email
+    
+    spanUser.textContent = "";  //faz o span ficar vazio
+    spanSenha.textContent = ""; //faz o span ficar vazio
+    spanEmail.textContent = ""; //faz o span ficar vazio
+    if(form.usuario.value === ""){ //se o campo estiver vazio
+        spanUser.textContent = "Campo usuário não preenchido"; //mudar o span para avisar que esta vazio
+        formValido = false; //invalidar o form
+    }
+    if(form.senha.value === ""){ //se o campo estiver vazio
+        spanSenha.textContent = "Campo senha não preenchido"; //mudar o span para avisar que esta vazio
+        formValido = false; //invalidar o form
+    }
+    if(form.email.value === ""){ //se o campo estiver vazio
+        spanEmail.textContent = "Campo email não preenchido"; //mudar o span para avisar que esta vazio
+        formValido = false; //invalidar o form
+    }
+    return formValido; //retornar true ou false dependendo se o form eh valido ou nao
 }
