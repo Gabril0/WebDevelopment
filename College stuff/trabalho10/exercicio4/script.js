a0513d6e010762e49170396201c7a4bf5142880d
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-05-19 13:54:43 -0300

    Add files via upload

--- College stuff/trabalho10/exercicio4/script.js
+++ College stuff/trabalho10/exercicio4/script.js
@@ -1,52 +1,40 @@
-// Function to fetch stock quote
-function GetQuote(stockCode) {
+function getQuote(stock) {
     const apiKey = "E0D85AP6BN6H07H8";
-    const url = `https://api.example.com/quote?code=${stockCode}&apiKey=${apiKey}`;
-  
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}.SA&apikey=${apiKey}`;
+
     return fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("erro!!");
-        }
-      })
+      .then(response => response.json())
       .then(data => {
-        return { acao: stockCode, cotacao: data.price };
+        const quote = data["Global Quote"];
+        const symbol = quote["01. symbol"];
+        const price = quote["05. price"];
+        return { acao: symbol, cotacao: price };
       });
   }
-  
+  //criação e atribuição dinamica dos parágrafos com seus valores
+  function renderStock(stock) {
+    const quoteResults = document.getElementById("quoteResults");
+    const stockElement = document.createElement("p");
+    stockElement.textContent = `Cotação da Ação ${stock.acao}: ${stock.cotacao}`;
+    quoteResults.appendChild(stockElement);
+  }
+  //faz sequencialmente
+  function fetchSequencial() {
+    const stocks = ["ABEV3", "ASAI3", "AZUL4", "B3SA3", "BIDI11"];
 
-  function RenderStock(stock) {
-    const stockInfo = document.createElement("p");
-    stockInfo.textContent = `Stock: ${stock.acao}, Quote: ${stock.cotacao}`;
-    document.body.appendChild(stockInfo);
+    const sequence = stocks.reduce((promise, stock) => {
+      return promise.then(() => getQuote(stock)).then(renderStock);
+    }, Promise.resolve());
+  }
+  //espera todas terminarem para fazer por conta do promise all
+  function fetchAssincrono() {
+    const stocks = ["ABEV3", "ASAI3", "AZUL4", "B3SA3", "BIDI11"];
+
+    const promises = stocks.map(stock => getQuote(stock));
+    Promise.all(promises).then(quotes => {
+      quotes.forEach(renderStock);
+    });
   }
   
-
-  function handleSequentialQuoting() {
-    const inputElements = document.querySelectorAll("input[name='stockCode']");
-    const stockCodes = Array.from(inputElements).map(input => input.value);
-  
-    function processNextStock(index) {
-      if (index >= stockCodes.length) {
-        return; 
-      }
-  
-      GetQuote(stockCodes[index])
-        .then(stock => {
-          RenderStock(stock);
-          processNextStock(index + 1); 
-        })
-        .catch(error => {
-          console.error(error);
-          processNextStock(index + 1); 
-        });
-    }
-  
-    processNextStock(0); 
-  }
-  
-  const cotarButton = document.getElementById("cotarButton");
-  cotarButton.addEventListener("click", handleSequentialQuoting);
-  
+  // Chamar a função para buscar as cotações sequencialmente ao carregar a página
+  fetchSequencial();
